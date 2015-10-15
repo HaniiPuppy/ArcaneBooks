d4d144fb8889f5f6767df6cfd1f9daff5ac596cc
Cameron <cmcleod18@hotmail.com>
2015-10-15 19:26:04 -0400

    Project Initial Commit.

---
+++ arcanebooks/java/com/haviitsu/arcanebooks/ArcaneBooks.java
@@ -0,0 +1,36 @@
+package com.haviitsu.arcanebooks;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by Tatsu on 10/15/2015.
+ */
+@Mod(modid = "ArcaneBooks", name = "Arcane Books", version = "1.7.10-1.0")
+public class ArcaneBooks {
+
+    @Mod.Instance("ArcaneBooks")
+    public static ArcaneBooks instance;
+
+
+    @Mod.EventHandler
+    public void PreInitializationEvent(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void InitializationEvent(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void PostInitializationEvent(FMLPostInitializationEvent event)
+    {
+
+    }
+
+}
